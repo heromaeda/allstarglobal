754344e592a6e4803c230e8fbe8defd3566f5838
heromaeda <hiro@allstarsaas.com>
2024-12-31 23:24:19 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/your-repo-name'
+}
